743747f9cbd7f02b8c7039700cb718097f36cb5a
Jason Livesay <runvnc@gmail.com>
2020-09-07 02:19:19 -0700

    opengl

--- endofdockerfile.sh
+++ endofdockerfile.sh
@@ -6,6 +6,7 @@ echo -e "${G}Updating apt db.."
 apt-get update
 echo -e "${G}Installing apt packages.."
 apt-get install -y wget cmake build-essential vim htop git xfce4 xfce4-goodies cuda-toolkit-10-0
+apt-get install -y sudo apt-get install libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev
 echo -e "${G}Installing VirtualGL.."
 wget https://sourceforge.net/projects/virtualgl/files/2.6.4/virtualgl_2.6.4_amd64.deb
 dpkg -i virtualgl_2.6.4_amd64.deb
