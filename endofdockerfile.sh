b11526dd0dcbbaf75ba4bd67cf4398dd12b2832e
root <runvnc@gmail.com>
2020-09-12 05:00:28 -0700

    dir

--- endofdockerfile.sh
+++ endofdockerfile.sh
@@ -12,10 +12,12 @@ echo -e "${G}Installing VirtualGL..${N}"
 wget https://sourceforge.net/projects/virtualgl/files/2.6.4/virtualgl_2.6.4_amd64.deb
 dpkg -i virtualgl_2.6.4_amd64.deb
 echo -e "${G}Installing fish shell.. ${N}"
+apt install -y libncurses5-dev libncursesw5-dev
 wget https://github.com/fish-shell/fish-shell/releases/download/3.1.2/fish-3.1.2.tar.gz
 tar xvf fish-3.1.2.tar.gz
 cd fish-3.1.2
 cmake .; make; make install
+cd ..
 echo -e "${G}Installing TurboVNC..${N}"
 wget https://sourceforge.net/projects/turbovnc/files/2.2.5/turbovnc_2.2.5_amd64.deb
 dpkg -i turbovnc_2.2.5_amd64.deb
